d9d260efaafb317391b1639dc9b91ec58b78b959
Josh Megnauth <joshua.megnauth54@gmail.com>
2022-08-08 22:02:14 -0400

    Mostly fix and modernize typing.

--- eviction_analysis/etl_evict.py
+++ eviction_analysis/etl_evict.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
+import numpy.typing as npt
 import logging
 from pathlib import Path
-from typing import Union, Optional
+from os import PathLike
+from typing import Optional, Literal
+from pandas._typing import DtypeArg
 
 ## Online paths
 # Eviction lab data: https://evictionlab.org/
@@ -21,7 +25,7 @@ NYC_BOROUGH: Path = DATA_DIR.joinpath("nyc_zip_borough_neighborhoods_pop.csv")
 
 
 def load_eviction(
-    path: Union[str, Path] = EVICTION_REL,
+    path: str | PathLike[str] = EVICTION_REL,
     date_col: str = "month",
     pyarrow: bool = False,
 ) -> pd.DataFrame:
@@ -29,7 +33,7 @@ def load_eviction(
 
     Parameters
     ----------
-    path: Union[str, Path]
+    path: str | PathLike[str]
         Path to Eviction Lab data as a CSV.
     date_col: str
         Variable to parse as a date. Defaults to "month."
@@ -45,7 +49,7 @@ def load_eviction(
     logging.info(f"Loading Eviction Lab data set from: {path}")
 
     ## read_csv arguments
-    dtype: dict[str, str] = {
+    dtype: DtypeArg = {
         "city": "category",
         "racial_majority": "category",
         "type": "category",
@@ -54,7 +58,7 @@ def load_eviction(
     # I'm biased. I like Arrow.
     # infer_datetime_format is only enabled if Arrow is disabled because PyArrow doesn't support it yet.
     # Likewise, low_memory is enabled with PyArrow and disabled otherwise.
-    engine: bool = "pyarrow" if pyarrow else None
+    engine: Optional[Literal["pyarrow"]] = "pyarrow" if pyarrow else None
     # GEOID's missing zip codes are tagged as "sealed" but they should be NaNs.
     na_values: dict[str, str] = {"GEOID": "sealed"}
     # Date columns
@@ -74,12 +78,12 @@ def load_eviction(
     return df
 
 
-def load_fmr(path: Union[str, Path], year: int) -> pd.DataFrame:
+def load_fmr(path: str | PathLike[str], year: int) -> pd.DataFrame:
     """Load and clean Fair Market Rate data set.
 
     Parameters
     ----------
-    path: Union[str, Path]
+    path: str | PathLike[str]
         Path to FMR data as an Excel file.
     year: int
         Year for path (FMR are issued per year).
@@ -96,8 +100,8 @@ def load_fmr(path: Union[str, Path], year: int) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_excel(path)
 
     # Rename the horrible Excel columns
-    names: List[str] = ["zipcode", "fmr_2br", "fmr_2br_90", "fmr_2br_110"]
-    usecols: pd.Series = df.columns.str.match(
+    names: list[str] = ["zipcode", "fmr_2br", "fmr_2br_90", "fmr_2br_110"]
+    usecols: npt.NDArray[np.bool_] = df.columns.str.match(
         r"(\d{0,4}\s{0,1}SAFMR\d{0,4}\n2BR)|(ZIP)|(zcta)|((SAFMR|safmr)\s{0,1}\d{2,4}\s2br)"
     )
 
@@ -105,7 +109,7 @@ def load_fmr(path: Union[str, Path], year: int) -> pd.DataFrame:
     # Excel files tend to suck.
     logging.debug(f"FMR usecols: {usecols}")
     logging.debug(f"FMR actual cols: {df.columns}")
-    df: pd.DataFrame = df.loc[:, usecols]
+    df = df.loc[:, usecols]
     df.columns = names
 
     # Convert zipcode to Int64 because some of the tracts are null
@@ -117,14 +121,14 @@ def load_fmr(path: Union[str, Path], year: int) -> pd.DataFrame:
     return df
 
 
-def load_zip_city(path: Union[str, Path] = ZIP_TRACT) -> pd.DataFrame:
+def load_zip_city(path: str | PathLike[str] = ZIP_TRACT) -> pd.DataFrame:
     """Write later."""
     logging.info(f"Loading zip code to census tract data from {path}")
 
     # Rename zip because it's a keyword and therefore annoying to use.
     # Rename usps-whatever because it's wordy.
     # Also...drop the rest of the columns
-    names: List[str] = ["zipcode", "tract", "city", "state"]
+    names: list[str] = ["zipcode", "tract", "city", "state"]
 
     zip_tract: pd.DataFrame = pd.read_excel(path, names=names, usecols=names)
     # Lower cased city will help with merges
@@ -134,13 +138,13 @@ def load_zip_city(path: Union[str, Path] = ZIP_TRACT) -> pd.DataFrame:
 
 
 def load_nyc_neighborhoods(
-    path: Union[str, Path] = NYC_BOROUGH, pyarrow: bool = True
+    path: str | PathLike[str] = NYC_BOROUGH, pyarrow: bool = True
 ) -> pd.DataFrame:
     """Load New York City neighborhoods data.
 
     Parameters
     ----------
-    path: Union[str, Path]
+    path: str | PathLike[str]
         Path to betaNYC neighborhoods data. Defaults to a locally cached copy.
     pyarrow: bool
         Use Apache Arrow. Defaults to True because PyArrow supports every
@@ -153,7 +157,7 @@ def load_nyc_neighborhoods(
     """
     logging.info(f"Loading New York City neighborhoods data from {path}")
 
-    dtype: dict[str, str] = {
+    dtype: DtypeArg = {
         "zip": "Int64",
         "borough": "category",
         "post_office": "category",
@@ -170,8 +174,8 @@ def load_nyc_neighborhoods(
 
 def merge_evic_fmr(
     evictions: pd.DataFrame,
-    cities: Optional[Union[list[str], str]] = "New York, NY",
-    neighborhoods: Optional[list[pd.DataFrame]] = None,
+    cities: Optional[list[str] | str] = "New York, NY",
+    neighborhoods: Optional[pd.DataFrame | list[pd.DataFrame]] = None,
 ) -> pd.DataFrame:
     """Write later."""
     logging.info("Merging data sets into the Eviction Labs DataFrame")
@@ -180,17 +184,17 @@ def merge_evic_fmr(
     # If city is a single string then add it to a list for filtering
     if isinstance(cities, str):
         logging.info("Enabled: filtering on cities")
-        cities: list[str] = [cities]
+        cities = [cities]
 
     # Likewise for neighborhoods
     if isinstance(neighborhoods, pd.DataFrame):
         logging.info("Enabled: merging neighborhoods into Eviction Labs")
-        neighborhoods: list[pd.DataFrame] = [neighborhoods]
+        neighborhoods = [neighborhoods]
 
     # Filter on cities if requested
     if cities:
-        evictions: pd.DataFrame = evictions.loc[evictions.city.isin(cities), :]
-        evictions: pd.DataFrame = evictions.reset_index(drop=True)
+        evictions = evictions.loc[evictions.city.isin(cities), :]
+        evictions = evictions.reset_index(drop=True)
 
     # Temporary, lower cased city names as well as states to ease merging
     # city_state: pd.DataFrame = evictions.city.str.extract(r"^([\w\s]+),\s(\w+)$").apply(
@@ -210,16 +214,16 @@ def merge_evic_fmr(
     if cities:
         # City, State is separated into features for zip_tract so I have to
         # replicate the split to filter
-        city_temp: list[tuple(str, str)] = [city.lower().split(", ") for city in cities]
+        city_temp: list[list[str]] = [city.lower().split(", ") for city in cities]
         city_zip: list[str] = [city[0] for city in city_temp]
         state_zip: list[str] = [city[1] for city in city_temp]
-        zip_tract: pd.DataFrame = zip_tract.loc[
+        zip_tract = zip_tract.loc[
             zip_tract.city.isin(city_zip) & zip_tract.state.isin(state_zip)
         ]
 
     # Fair market rate data
     fmr_years: list[int] = [2020, 2021, 2022]
-    fmr_paths: Path = [
+    fmr_paths: list[Path] = [
         DATA_DIR.joinpath(f"fy{year}_safmrs_revised.xlsx") for year in fmr_years
     ]
     fmrs: list[pd.DataFrame] = [
@@ -228,14 +232,12 @@ def merge_evic_fmr(
 
     # Filter FMRs by zip code if cities were provided
     if cities:
-        fmrs: pd.DataFrame = [
-            fmr.loc[fmr.zipcode.isin(zip_tract.zipcode), :] for fmr in fmrs
-        ]
+        fmrs = [fmr.loc[fmr.zipcode.isin(zip_tract.zipcode), :] for fmr in fmrs]
 
     # Merge FMR data sets with zipcode/tracts and concat all of the DataFrames
     # Melting the DataFrame is primarily so zip codes and census tract are in
     # one variable to merge on Eviction Labs.
-    fmrs: list[pd.DataFrame] = [
+    fmrs = [
         fmr.merge(zip_tract, on="zipcode", how="left").melt(
             ["fmr_2br", "fmr_2br_90", "fmr_2br_110", "fmr_year", "city", "state"],
             var_name="tabulation",
@@ -243,18 +245,21 @@ def merge_evic_fmr(
         )
         for fmr in fmrs
     ]
-    fmrs: pd.DataFrame = pd.concat(fmrs)
+    fmrs_merged: pd.DataFrame = pd.concat(fmrs)
 
     # FMRs are merged on zip code and year to produce a long DataFrame where
     # the dates match up
-    evictions: pd.DataFrame = evictions.merge(
-        fmrs, left_on=["geoid", "temp_year"], right_on=["code", "fmr_year"], how="left"
+    evictions = evictions.merge(
+        fmrs_merged,
+        left_on=["geoid", "temp_year"],
+        right_on=["code", "fmr_year"],
+        how="left",
     )
 
     # Merge neighborhoods data
     if neighborhoods:
         for neighborhood in neighborhoods:
-            evictions: pd.DataFrame = evictions.merge(
+            evictions = evictions.merge(
                 neighborhood, left_on="geoid", right_on="zipcode", how="left"
             )
 
